9926b90552fbaf26fb438624071bb1f482a88aaa
muastani <s64309010016@ptnicec.ac.th>
2022-10-21 11:50:28 +0700

    ส่งไฟล์ comment

---
+++ comment.php
@@ -0,0 +1,14 @@
+<?php
+
+  //บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+echo "Muas Bakrirangsan<br>";
+echo "Information Technology<br>";
+//echo "Pattani<br>";
+#echo "Thailand<br>";
+
+  //บรรทัดที่ 10-11 แสดงผลการใช้งาน Comment แบบหลายบรรทัด
+/* echo "94000<br>";
+echo "Asia<br>";*/
+echo "Universal<br>"; 
+
+?>
